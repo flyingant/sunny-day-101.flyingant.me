79cea707de5aac390237edf48301eb5717061772
mayi <mayi@wiredcraft.com>
2019-12-10 12:13:57 +0800

    fixed the problem when on the mobile browser

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,1 @@
-const playBtn = document.getElementsByClassName('play');
-const box = document.getElementsByClassName('box');
-playBtn[0].onclick = () => {
-  box[0].innerHTML = `<audio autoplay><source src="../assets/hammerhammerhammer.mp3" type="audio/mpeg"></source></audio>`;
-};
+console.log('MaYi: Hi, there!');
