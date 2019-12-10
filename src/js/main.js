aa192c950c8ee956432d77ae90b62c6edea302bb
mayi <mayi@wiredcraft.com>
2019-12-10 11:11:47 +0800

    download the sound track to the local to fixed the problem on mobile devices

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 const playBtn = document.getElementsByClassName('play');
 const box = document.getElementsByClassName('box');
 playBtn[0].onclick = () => {
-  box[0].innerHTML = `<iframe width="100%" height="240" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/40373559&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>`;
+  box[0].innerHTML = `<audio autoplay><source src="../assets/hammerhammerhammer.mp3" type="audio/mpeg"></source></audio>`;
 };
